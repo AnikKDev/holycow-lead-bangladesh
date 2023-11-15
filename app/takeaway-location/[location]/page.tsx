cdd39a0dd314828fe034a2698d8bffff585f7d26
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-11-15 20:51:25 +0600

    location details - name, review, address, phone

--- app/takeaway-location/[location]/page.tsx
+++ app/takeaway-location/[location]/page.tsx
@@ -1,9 +1,11 @@
+import AboutLocation from '@/components/menu-page/about-location'
 import CoverPhoto from '@/components/menu-page/cover-photo'
 
 const TakeawayLocationMenuPage = () => {
 	return (
 		<>
 			<CoverPhoto />
+			<AboutLocation />
 		</>
 	)
 }
--- components/menu-page/about-location.tsx
+++ components/menu-page/about-location.tsx
@@ -1,5 +1,23 @@
+import { MdOutlineLocationOn, MdOutlineStar } from 'react-icons/md'
+
 const AboutLocation = () => {
-	return <div>MenuInformation</div>
+	return (
+		<div className='container flex flex-col gap-2 text-foreground'>
+			<h1 className='mt-4 text-3xl font-bold'>Holycow - Archway</h1>
+			<div className='flex flex-col justify-center gap-1'>
+				<div className='flex items-center gap-1'>
+					<MdOutlineStar size={18} />
+					<h2 className='font-semibold'>4.8 (100+ ratings) • Takeaway </h2>
+				</div>
+				<div className='flex items-center gap-1'>
+					<MdOutlineLocationOn size={18} />
+					<h2 className='font-semibold'>
+						87 Junction Road Archway London N19 5QU • 0207 837 3473
+					</h2>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AboutLocation
