d81b72010e71ef0857cd929c14ca66538818508c
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-11-18 22:48:31 +0600

    menu item popup modal component

--- components/menu-page/main-menu/menu-item.tsx
+++ components/menu-page/main-menu/menu-item.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import itemImg from '@/public/menu-item.jpg'
 
+import { MenuItemModal } from './menu-item-modal'
 import { MenuItemType } from './menu-items'
 
 interface IMenuItem {
@@ -8,25 +13,40 @@ interface IMenuItem {
 }
 
 const MenuItem = ({ item }: IMenuItem) => {
+	const [showModal, setShowModal] = useState(false)
+	const router = useRouter()
 	return (
-		<div className='overflow-hidden rounded-md border border-border/75 bg-white transition hover:border-border/100 hover:shadow-[0px_10px_36px_0px_rgba(0,0,0,0.16)]'>
-			<div className='grid grid-cols-16 justify-between gap-2'>
-				<div className='col-start-1 col-end-12 flex flex-col gap-2 py-3 pl-2'>
-					<h2 className='text-base font-medium text-foreground'>{item.name}</h2>
-					<p className='text-sm font-normal text-[#767676] text-foreground'>
-						{item.description}
-					</p>
-					<p className='text-sm font-medium text-foreground/70'>{item.price}</p>
-				</div>
-				<div className='col-start-12 col-end-[17]'>
-					<Image
-						src={itemImg}
-						className='h-full w-full object-cover'
-						alt='Menu Item'
-					/>
+		<>
+			<div
+				tabIndex={0}
+				onClick={() => {
+					setShowModal(!showModal)
+				}}
+				className='cursor-pointer overflow-hidden rounded-md border border-border/75 bg-white transition hover:border-border/100 hover:shadow-[0px_10px_36px_0px_rgba(0,0,0,0.16)]'
+			>
+				<div className='grid grid-cols-16 justify-between gap-2'>
+					<div className='col-start-1 col-end-12 flex flex-col gap-2 py-3 pl-2'>
+						<h2 className='text-base font-medium text-foreground'>
+							{item.name}
+						</h2>
+						<p className='text-sm font-normal text-[#767676] text-foreground'>
+							{item.description}
+						</p>
+						<p className='text-sm font-medium text-foreground/70'>
+							{item.price}
+						</p>
+					</div>
+					<div className='col-start-12 col-end-[17]'>
+						<Image
+							src={itemImg}
+							className='h-full w-full object-cover'
+							alt='Menu Item'
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+			<MenuItemModal showModal={showModal} setShowModal={setShowModal} />
+		</>
 	)
 }
 
