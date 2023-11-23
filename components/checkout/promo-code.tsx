edbf76c372ce535968a977818997eb6f544b446d
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-11-23 21:05:18 +0600

    checkout promo code section

--- components/checkout/promo-code.tsx
+++ components/checkout/promo-code.tsx
@@ -1,5 +1,25 @@
+import { IoPricetagOutline } from 'react-icons/io5'
+
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+
 const PromoCodeApply = () => {
-	return <div>PromoCodeApply</div>
+	return (
+		<form>
+			<div className='grid grid-cols-[auto,1fr] gap-2.5'>
+				<div>
+					<IoPricetagOutline size={22} className='text-foreground' />
+				</div>
+				<div className='flex flex-col gap-2'>
+					<h3 className='text-base font-medium'>Promo Code</h3>
+					<div className='flex w-full items-center space-x-2'>
+						<Input placeholder='Enter promo code' className='bg-white' />
+						<Button type='submit'>Apply</Button>
+					</div>
+				</div>
+			</div>
+		</form>
+	)
 }
 
 export default PromoCodeApply
