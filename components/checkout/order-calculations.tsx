109edc7dd03a798d37888268a4bf3b8e496d6b36
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-11-23 22:43:27 +0600

    order calculation area

--- components/checkout/order-calculations.tsx
+++ components/checkout/order-calculations.tsx
@@ -1,5 +1,20 @@
 const OrderCalculations = () => {
-	return <div>OrderCalculations</div>
+	return (
+		<div className='flex flex-col gap-1.5'>
+			<div className='flex items-center justify-between'>
+				<h3 className='text-base font-medium'>Subtotal</h3>
+				<span className='text-base font-medium'>30.69</span>
+			</div>
+			<div className='flex items-center justify-between'>
+				<h3 className='text-base font-medium'>Delivery Fee</h3>
+				<span className='text-base font-medium'>1.50</span>
+			</div>
+			<div className='flex items-center justify-between'>
+				<h3 className='text-base font-medium'>Discount</h3>
+				<span className='text-base font-medium'>-5.00</span>
+			</div>
+		</div>
+	)
 }
 
 export default OrderCalculations
