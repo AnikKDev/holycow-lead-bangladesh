de4f56a09aa4cd13e4854576ab257c4f647bd9de
anikdev2016 <anikdev2016@gmail.com>
2023-11-24 22:03:52 +0600

    completed homepage

--- components/home-components/restaurant-dialog-contents.tsx
+++ components/home-components/restaurant-dialog-contents.tsx
@@ -1,4 +1,10 @@
+'use client'
+
 import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+import * as z from 'zod'
 
 import {
 	Dialog,
@@ -9,43 +15,77 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog'
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 import { Button } from '../ui/button'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '../ui/select'
 
 type Props = {}
 
 export default function RestaurantsDialogContents({}: Props) {
+	const FormSchema = z.object({
+		locations: z.string({}),
+	})
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+	})
+	function onSubmit(data: z.infer<typeof FormSchema>) {
+		console.log(data)
+	}
 	return (
 		<DialogContent className='sm:max-w-[425px]'>
-			<DialogHeader>
-				<DialogTitle>Edit profile</DialogTitle>
-				<DialogDescription>
-					Make changes to your profile here. Click save when you're done.
-				</DialogDescription>
-			</DialogHeader>
-			<div className='grid gap-4 py-4'>
-				<div className='grid grid-cols-4 items-center gap-4'>
-					<Label htmlFor='name' className='text-right'>
-						Name
-					</Label>
-					<Input id='name' defaultValue='Pedro Duarte' className='col-span-3' />
-				</div>
-				<div className='grid grid-cols-4 items-center gap-4'>
-					<Label htmlFor='username' className='text-right'>
-						Username
-					</Label>
-					<Input
-						id='username'
-						defaultValue='@peduarte'
-						className='col-span-3'
+			{/* dropdown selector */}
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='w-full space-y-6'
+				>
+					<FormField
+						control={form.control}
+						name='locations'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='text-2xl'>
+									Select the nearest takeaway location
+								</FormLabel>
+								<Select
+									onValueChange={field.onChange}
+									defaultValue={field.value}
+								>
+									<FormControl>
+										<SelectTrigger>
+											<SelectValue placeholder='Select takeaway location' />
+										</SelectTrigger>
+									</FormControl>
+									<SelectContent>
+										<SelectItem value='angel'>Angel</SelectItem>
+										<SelectItem value='archway'>Archway</SelectItem>
+										<SelectItem value='balham'>Balham</SelectItem>
+										<SelectItem value='battersea'>Battersea</SelectItem>
+									</SelectContent>
+								</Select>
+								<FormMessage />
+							</FormItem>
+						)}
 					/>
-				</div>
-			</div>
-			<DialogFooter>
-				<Button type='submit'>Save changes</Button>
-			</DialogFooter>
+				</form>
+			</Form>
 		</DialogContent>
 	)
 }
