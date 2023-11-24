86d782243a6c5a7c170d7867c0a18a5cc9d6bf13
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-11-24 21:45:03 +0600

    collection address and collection time

--- components/checkout/collection/collection-time.tsx
+++ components/checkout/collection/collection-time.tsx
@@ -1,5 +1,23 @@
+import { GoClock } from 'react-icons/go'
+
+import { Button } from '@/components/ui/button'
+
 const CollectionTimeSection = () => {
-	return <div>CollectionTimeSection</div>
+	return (
+		<div className='flex items-center justify-between'>
+			<div className='flex items-center gap-2.5'>
+				<GoClock size={22} className='text-foreground' />
+				<div>
+					<h3 className='text-base font-medium'>Collection Time</h3>
+				</div>
+			</div>
+			<div>
+				<Button className='h-7 rounded-full px-5' size='sm'>
+					Schedule
+				</Button>
+			</div>
+		</div>
+	)
 }
 
 export default CollectionTimeSection
