ae1ccc7dc16a31b4f23bbdcaaecb27106325285b
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-11-30 01:27:55 +0600

    reservation tab ui completed

--- components/menu-page/reservation/reservation-modal.tsx
+++ components/menu-page/reservation/reservation-modal.tsx
@@ -2,14 +2,14 @@
 
 import { Dispatch, SetStateAction } from 'react'
 
-import { Button } from '@/components/ui/button'
 import {
 	Dialog,
 	DialogContent,
-	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 } from '@/components/ui/dialog'
+
+import ReservationTabContainer from './reservation-tab'
 
 export function ReservationModal({
 	showModal,
@@ -22,21 +22,15 @@ export function ReservationModal({
 		<>
 			<Dialog open={showModal} onOpenChange={setShowModal}>
 				<DialogContent className='sm:max-w-[425px] max-w-2xl overflow-hidden p-0'>
-					<DialogHeader className='px-5 pb-5 pt-5 text-left'>
-						<DialogTitle className='pt-3 text-xl'>
+					<DialogHeader className='px-5 pt-5 text-left'>
+						<DialogTitle className='pt-4 text-2xl'>
 							Booking at Holycow - Putney
 						</DialogTitle>
 					</DialogHeader>
 
-					<DialogFooter className='mt-2 flex-col gap-2 pb-4 pt-4'>
-						<div className='px-5'>
-							<div className='flex w-full items-center gap-2.5'>
-								<Button variant='default' size='lg' className='w-full'>
-									Submit
-								</Button>
-							</div>
-						</div>
-					</DialogFooter>
+					<div className='px-5 pt-2'>
+						<ReservationTabContainer />
+					</div>
 				</DialogContent>
 			</Dialog>
 		</>
--- components/menu-page/reservation/reservation-tab.tsx
+++ components/menu-page/reservation/reservation-tab.tsx
@@ -1,5 +1,74 @@
-const ReservationTab = () => {
-	return <div>ReservationTab</div>
+import { useState } from 'react'
+
+import { cn } from '@/lib/utils'
+
+import ContactInformationTab from './contact-info-tab'
+import FindATableTab from './find-table-tab'
+import PaymentTab from './payment-tab'
+
+const ReservationTabContainer = () => {
+	const [tab, setTab] = useState<'find_table' | 'contact_info' | 'payment'>(
+		'find_table'
+	)
+
+	const onTabChange = (value) => {
+		setTab(value)
+	}
+
+	return (
+		<div>
+			<div className='flex w-full items-center space-x-7 shadow-[rgb(204,204,204)_0px_-1px_0px_inset]'>
+				<button
+					className={cn(
+						'cursor-auto border-b-2 border-b-transparent pb-3 text-sm font-medium uppercase tracking-[1px] transition',
+						{
+							'border-primary text-primary': tab === 'find_table',
+						}
+					)}
+					onClick={() => {
+						setTab('find_table')
+					}}
+				>
+					Find a Table
+				</button>
+				<button
+					className={cn(
+						'cursor-auto border-b-2 border-b-transparent pb-3 text-sm font-medium uppercase tracking-[1px] transition',
+						{
+							'border-primary text-primary': tab === 'contact_info',
+						}
+					)}
+					onClick={() => {
+						setTab('contact_info')
+					}}
+				>
+					Contact Information
+				</button>
+				<button
+					className={cn(
+						'cursor-auto border-b-2 border-b-transparent pb-3 text-sm font-medium uppercase tracking-[1px] transition',
+						{
+							'border-primary text-primary': tab === 'payment',
+						}
+					)}
+					onClick={() => {
+						setTab('payment')
+					}}
+				>
+					Payment
+				</button>
+			</div>
+			<div className='pb-5 pt-4'>
+				{tab === 'find_table' ? (
+					<FindATableTab />
+				) : tab === 'contact_info' ? (
+					<ContactInformationTab />
+				) : (
+					tab === 'payment' && <PaymentTab />
+				)}
+			</div>
+		</div>
+	)
 }
 
-export default ReservationTab
+export default ReservationTabContainer
