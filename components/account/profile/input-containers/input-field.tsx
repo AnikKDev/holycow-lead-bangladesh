b94c2c693a014328e782f99e053d4e4f27206340
anikkdev <anikdev2016@gmail.com>
2023-11-29 15:17:22 +0600

    completed profile page

---
+++ components/account/profile/input-containers/input-field.tsx
@@ -0,0 +1,59 @@
+import React, { SetStateAction } from 'react'
+
+import EmailInput from '../../account-components/input-fields/email-input'
+import PasswordInput from '../../account-components/input-fields/password-input'
+import TextInput from '../../account-components/input-fields/text-input'
+
+type Props = {
+	setEditingSection: React.Dispatch<SetStateAction<string | null>>
+	editingSection: string | null
+}
+
+export default function InputField({
+	setEditingSection,
+	editingSection,
+}: Props) {
+	let displayContent
+	if (editingSection.toLowerCase() === 'name') {
+		displayContent = (
+			<TextInput
+				id='full_name'
+				label='Full Name'
+				type='text'
+				key={'full_name'}
+				name='name'
+				setEditingSection={setEditingSection}
+			/>
+		)
+	} else if (editingSection.toLowerCase() === 'email') {
+		displayContent = (
+			<EmailInput
+				id='email'
+				label='Email'
+				type='email'
+				key={'email'}
+				name='email'
+				setEditingSection={setEditingSection}
+			/>
+		)
+	} else if (editingSection.toLowerCase() === 'password') {
+		displayContent = (
+			<PasswordInput
+				id='password'
+				label='Password'
+				type='password'
+				key={'password'}
+				name='password'
+				setEditingSection={setEditingSection}
+			/>
+		)
+	}
+	return (
+		<div className='p-6'>
+			<h4 className='mb-5 text-2xl font-bold capitalize'>
+				Edit {editingSection}
+			</h4>
+			{displayContent}
+		</div>
+	)
+}
