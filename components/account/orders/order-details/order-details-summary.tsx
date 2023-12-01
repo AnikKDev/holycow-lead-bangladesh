f93e62662a4a27466319a16da8241b50e4b35055
anikkdev <anikdev2016@gmail.com>
2023-12-01 18:22:16 +0600

    completed items section

--- components/account/orders/order-details/order-details-summary.tsx
+++ components/account/orders/order-details/order-details-summary.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import OrderDetailsSummaryItems from './order-details-summary-items'
+
 type Props = {}
 
 export default function OrderDetailsSummary({}: Props) {
-	return <div>OrderDetailsSummary</div>
+	return (
+		<div>
+			<h3 className='my-8 text-xl font-semibold'>Order Summary</h3>
+			<OrderDetailsSummaryItems />
+			<OrderDetailsSummaryItems />
+			<OrderDetailsSummaryItems />
+		</div>
+	)
 }
