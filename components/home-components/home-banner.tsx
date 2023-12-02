e49014ff884e6c576af288723f281ec9055602ae
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-12-02 22:58:25 +0600

    utilized reusable component for showing homepage takeaway/restaurant locations

--- components/home-components/home-banner.tsx
+++ components/home-components/home-banner.tsx
@@ -1,61 +1,59 @@
-import React from 'react'
-import home_banner from '@/assets/homepage/home_banner.png'
+'use client'
+
+import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 
-import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
-} from '../ui/select'
-import RestaurantsDialogContents from './restaurant-dialog-contents'
+import { allRestaurantLocations, allTakeawayLocations } from './date'
+import { HomeShowLocationsModal } from './home-modal'
 
 type Props = {}
 
 export default function HomeBanner({}: Props) {
+	const [showTakeawayLocations, setShowTakeawayLocations] = useState(false)
+	const [showRestaurantLocations, setShowRestaurantLocations] = useState(false)
+
 	return (
-		<div className='flex h-screen flex-col items-center justify-center'>
-			<div className='home-banner_background h-screen w-full  bg-cover bg-center'>
-				<div className='flex h-full w-full items-center justify-center  backdrop-blur-sm backdrop-brightness-50 mobile-sm:flex-col'>
-					<Button
-						size='lg'
-						className='mx-3 my-3 w-72 rounded-3xl text-xl transition-all hover:bg-white hover:text-primary'
-						variant='default'
-					>
-						Takeway
-					</Button>
-					<Dialog>
-						<DialogTrigger asChild>
-							<Button
-								size='lg'
-								className='mx-3 w-72 rounded-3xl text-xl transition-all hover:bg-white hover:text-primary'
-								variant='default'
-							>
-								Restaurants
-							</Button>
-						</DialogTrigger>
-						{/* restaurant dialog contents */}
-						<DialogContent className='sm:max-w-[425px]'>
-							<RestaurantsDialogContents />
-						</DialogContent>
-					</Dialog>
+		<>
+			<div className='flex h-screen flex-col items-center justify-center'>
+				<div className='home-banner_background h-screen w-full  bg-cover bg-center'>
+					<div className='flex h-full w-full items-center justify-center  backdrop-blur-sm backdrop-brightness-50 mobile-sm:flex-col'>
+						<Button
+							onClick={() => {
+								setShowTakeawayLocations(true)
+							}}
+							size='lg'
+							className='mx-3 my-3 w-72 rounded-3xl text-xl transition-all hover:bg-white hover:text-primary'
+							variant='default'
+						>
+							Takeway
+						</Button>
+						<Button
+							onClick={() => {
+								setShowRestaurantLocations(true)
+							}}
+							size='lg'
+							className='mx-3 w-72 rounded-3xl text-xl transition-all hover:bg-white hover:text-primary'
+							variant='default'
+						>
+							Restaurants
+						</Button>
+					</div>
 				</div>
 			</div>
-		</div>
+			<HomeShowLocationsModal
+				header='Select the nearest takeaway location'
+				selectOptions={allTakeawayLocations}
+				showModal={showTakeawayLocations}
+				setShowModal={setShowTakeawayLocations}
+			/>
+
+			<HomeShowLocationsModal
+				header='Select the nearest restaurant location'
+				selectOptions={allRestaurantLocations}
+				showModal={showRestaurantLocations}
+				setShowModal={setShowRestaurantLocations}
+			/>
+		</>
 	)
 }
