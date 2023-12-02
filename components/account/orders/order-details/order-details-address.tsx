30840ec99182ef96779b04d7a7dca0546a5e4237
anikkdev <anikdev2016@gmail.com>
2023-12-02 18:58:07 +0600

    completed steppers

--- components/account/orders/order-details/order-details-address.tsx
+++ components/account/orders/order-details/order-details-address.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function OrderDetailsAddress({}: Props) {
 	return (
 		<div>
-			<h3 className='mb-3 mt-8 text-2xl font-semibold capitalize'>
+			<h3 className='mb-3 mt-8 text-2xl font-medium capitalize'>
 				Delivery address/pickup address
 			</h3>
 			<h4 className='mb-3 text-xl font-medium capitalize text-gray-500'>
--- components/account/orders/order-details/order-details-status.tsx
+++ components/account/orders/order-details/order-details-status.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
 
+import CustomBadge from '@/components/ui/custom-ui/custom-badge'
+
+import OrderDetailsStatusSteps from './order-details-status-steps'
+
 type Props = {}
 
 export default function OrderDetailsStatus({}: Props) {
-	return <div>OrderDetailsStatus</div>
+	const steps = [
+		{ title: 'Order placed', description: 'Some desc text', completed: true },
+		{
+			title: 'Order being prepeared',
+			description: '3:54 PM, 18/05/2022',
+			completed: true,
+		},
+		{
+			title: 'Out for Delivery',
+			description: '3:54 PM, 18/05/2022',
+			completed: true,
+		},
+		{ title: 'Delivered', description: '--', completed: false },
+	]
+	return (
+		<div className='my-10 min-h-[4rem] rounded-lg border border-[#D1D5DB] p-6'>
+			<div>
+				<h3 className='mb-3 text-xl font-medium'>Order status</h3>
+				<CustomBadge status='In Progress' />
+			</div>
+			{/* steppers */}
+			<ol className='c-stepper mt-5'>
+				{steps.map((step, index) => (
+					<OrderDetailsStatusSteps
+						key={index}
+						title={step.title}
+						description={step.description}
+						completed={step.completed}
+					/>
+				))}
+			</ol>
+		</div>
+	)
 }
