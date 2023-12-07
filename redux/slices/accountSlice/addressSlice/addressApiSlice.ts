88ccd5c2beda1e0b5efac3a6729203d7706885b5
anikkdev <anikdev2016@gmail.com>
2023-12-07 21:42:49 +0600

    added mutation hooks for address

--- redux/slices/accountSlice/addressSlice/addressApiSlice.ts
+++ redux/slices/accountSlice/addressSlice/addressApiSlice.ts
@@ -8,11 +8,34 @@ export const addressApiSlice = apiSlice.injectEndpoints({
 				url: `/user/all_addresses/`,
 				method: 'GET',
 			}),
-			providesTags: (result, error, arg) => [{ type: 'Address', id: arg }],
+			providesTags: (result, error, arg) => [
+				'Address',
+				{ type: 'Address', id: arg },
+			],
+		}),
+		createAddress: builder.mutation({
+			query: (address) => ({
+				url: `/user/add_address/`,
+				method: 'POST',
+				body: address,
+			}),
+			invalidatesTags: ['Address'],
+		}),
+		updateAddress: builder.mutation({
+			query: (address) => ({
+				url: `/user/add_address/`,
+				method: 'PATCH',
+				body: address,
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: 'Address', id: arg }],
 		}),
 	}),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetAllAddressesQuery } = addressApiSlice
+export const {
+	useGetAllAddressesQuery,
+	useCreateAddressMutation,
+	useUpdateAddressMutation,
+} = addressApiSlice
