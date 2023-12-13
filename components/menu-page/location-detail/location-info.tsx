60a00d73f6382e037fb4d4cb8788371cc7d6bb74
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-12-13 21:55:41 +0600

    takeawey menu page responsiveness done

--- components/menu-page/location-detail/location-info.tsx
+++ components/menu-page/location-detail/location-info.tsx
@@ -11,16 +11,18 @@ const LocationInfo = ({
 }) => {
 	return (
 		<div className='flex flex-col gap-2.5'>
-			<h2 className='pb-2 text-lg font-semibold'>Where to find us</h2>
+			<h2 className='pb-2 text-lg font-semibold mobile-sm:text-base'>
+				Where to find us
+			</h2>
 			<div>
 				<Image src={mapImg} alt='Map' className='object-fit h-auto w-full' />
 			</div>
-			<div className='flex flex-col'>
-				<div className='flex items-center gap-2 border-b border-border py-4'>
+			<div className='flex flex-col text-base mobile-sm:text-sm'>
+				<div className='flex items-center gap-2 border-b border-border py-4 mobile-sm:py-3'>
 					<MdOutlineLocationOn size={18} />
 					<p>{locationInformation?.address}</p>
 				</div>
-				<div className='flex items-center gap-2 border-b border-border py-4'>
+				<div className='flex items-center gap-2 border-b border-border py-4 mobile-sm:py-3'>
 					<MdCall size={18} />
 					<p>
 						<a href={`tel:${locationInformation?.phone_number}`}>
@@ -28,7 +30,7 @@ const LocationInfo = ({
 						</a>
 					</p>
 				</div>
-				<div className='flex items-center gap-2 border-b border-border py-4'>
+				<div className='flex items-center gap-2 border-b border-border py-4 mobile-sm:py-3'>
 					<HiMail size={18} />
 					<p>{locationInformation?.email}</p>
 				</div>
--- components/menu-page/location-detail/location-opening-hours.tsx
+++ components/menu-page/location-detail/location-opening-hours.tsx
@@ -8,12 +8,12 @@ const LocationOpeningHours = ({
 }) => {
 	return (
 		<div className='flex flex-col gap-2.5'>
-			<h2 className='pb-2 text-lg font-semibold'>Opening Hours</h2>
+			<h2 className='pb-2 text-lg font-semibold '>Opening Hours</h2>
 
 			<div className='flex flex-col border-t border-border'>
 				{locationInformation.opening_hours.map((oh) => (
 					<div className='w-full border-b border-border py-4'>
-						<div className='flex w-full items-center justify-between px-4'>
+						<div className='flex w-full items-center justify-between px-4 text-base mobile-sm:px-3'>
 							<p>{oh.week_day}</p>
 							<p>
 								{format(
