143134a116ea2f53f4ba15932a84c5687fbd3607
anikdev2016 <anikdev2016@gmail.com>
2024-02-04 00:20:11 +0600

    craeted gradient wrapper

--- app/(v2)/private-event/page.tsx
+++ app/(v2)/private-event/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import bg from '@/assets/private-info/private-info-bg.png'
 
 import ContactForm from '@/components/v2/private-event/contact-form'
 import BannerContents from '@/components/v2/shared/banner-contents'
+import GradientWrapper from '@/components/v2/shared/gradient-wrapper'
 import PageInfo from '@/components/v2/shared/page-info'
 import SectionHeader from '@/components/v2/shared/SectionHeader'
 
@@ -9,21 +11,19 @@ type Props = {}
 
 export default function PrivateEvent({}: Props) {
 	return (
-		<section className='bg-gradient-to-b from-[#E8D3A2] via-[#F3F2EB] to-[#F3F2EB]'>
-			<div className='mx-auto w-[1327px] space-y-24'>
-				{/* banner here */}
-				<div>
-					<BannerContents text='Private event' />
-				</div>
-				{/* page info */}
-				<SectionHeader
-					subTitle='Make your event Simply Unforgettable'
-					title='Our dedicated events team helps you every step of the way. Intimate dinners, festive stand-up cocktail parties, unique corporate functions, and bespoke tasting experiences featuring sushi, sake offer endless possibilities!'
-					subTitleSize='small'
-				/>
-				{/* message to us form */}
-				<ContactForm />
+		<GradientWrapper>
+			{/* banner here */}
+			<div>
+				<BannerContents text='Private event' image={bg.src} />
 			</div>
-		</section>
+			{/* page info */}
+			<SectionHeader
+				subTitle='Make your event Simply Unforgettable'
+				title='Our dedicated events team helps you every step of the way. Intimate dinners, festive stand-up cocktail parties, unique corporate functions, and bespoke tasting experiences featuring sushi, sake offer endless possibilities!'
+				subTitleSize='small'
+			/>
+			{/* message to us form */}
+			<ContactForm />
+		</GradientWrapper>
 	)
 }
--- components/v2/shared/banner-contents.tsx
+++ components/v2/shared/banner-contents.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import bg from '@/assets/private-info/private-info-bg.png'
 
-type Props = { text: string }
+type Props = { text: string; image: string }
 
-export default function BannerContents({ text }: Props) {
+export default function BannerContents({ text, image }: Props) {
 	return (
 		<div className='mx-auto h-[480px] max-w-[1792px] '>
 			<div
 				style={{
-					background: `url(${bg.src}) `,
+					background: `url(${image}) `,
 					backgroundPosition: 'center',
 					backgroundSize: 'cover',
 				}}
 				className='h-full w-full overflow-hidden bg-no-repeat'
 			>
 				<div className='flex h-full w-full items-center justify-center bg-gradient-to-b from-[#0B0600]/70 to-[#4D412F]/10 bg-cover bg-no-repeat'>
-					<h4>{text}</h4>
+					<h1 className='text-center text-[64px] font-[600px] leading-[83.2px] tracking-[0.04em] text-[#DCCA87]'>
+						{text}
+					</h1>
 				</div>
 			</div>
 		</div>
