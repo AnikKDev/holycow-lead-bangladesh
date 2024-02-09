e16e9efe12e9aa879c00d019f700d4a2b96ac5d0
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-10 01:40:50 +0600

    Add breadcrumb in the banner component

--- components/v2/shared/Breadcrumb.tsx
+++ components/v2/shared/Breadcrumb.tsx
@@ -1,70 +1,44 @@
-// 'use client'
+'use client'
 
-// import React, { ReactNode } from 'react'
-// import Link from 'next/link'
-// import { usePathname } from 'next/navigation'
+import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { FaChevronRight } from 'react-icons/fa6'
 
-// type Props = {
-// 	homeElement: ReactNode
-// 	separator: ReactNode
-// }
+const Breadcrumb = () => {
+	const paths = usePathname()
+	const pathNames = paths.split('/').filter((path) => path)
+	return (
+		<div>
+			<ul className='flex items-center gap-2 font-lora text-lg font-semibold leading-[1.3] tracking-[0.04] text-[#EEDFC8]'>
+				<li>
+					<Link className='font-lora' href={'/'}>
+						Home
+					</Link>
+				</li>
+				{pathNames.length > 0}
+				{pathNames.map((link, index) => {
+					let href = `/${pathNames.slice(0, index + 1).join('/')}`
+					let itemLink = link.includes('-') ? link.split('-').join(' ') : link
 
-// const Breadcrumb = ({ homeElement, separator }: Props) => {
-// 	const paths = usePathname()
-// 	const pathNames = paths.split('/').filter((path) => path)
-// 	return (
-// 		<div>
-// 			<ul className='flex gap-2 font-lora text-lg font-semibold leading-[1.3] tracking-[0.04] text-[#EEDFC8]'>
-// 				<li>
-// 					<Link className='font-lora' href={'/'}>
-// 						{homeElement}
-// 					</Link>
-// 				</li>
-// 				{pathNames.length > 0 && separator}
-// 				{pathNames.map((link, index) => {
-// 					let href = `/${pathNames.slice(0, index + 1).join('/')}`
-// 					let itemLink = link.includes('-') ? link.split('-').join(' ') : link
+					return (
+						<React.Fragment key={index}>
+							<li>
+								<FaChevronRight />
+							</li>
+							<li>
+								<Link className='font-lora capitalize' href={href}>
+									{itemLink}
+								</Link>
+							</li>
 
-// 					return (
-// 						<React.Fragment key={index}>
-// 							<li>
-// 								<Link className='font-lora capitalize' href={href}>
-// 									{itemLink}
-// 								</Link>
-// 							</li>
-// 							{pathNames.length !== index + 1 && separator}
-// 						</React.Fragment>
-// 					)
-// 				})}
-// 			</ul>
-// 		</div>
-// 	)
-// }
+							{pathNames.length !== index + 1}
+						</React.Fragment>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
 
-// export default Breadcrumb
-// <section>
-// <div className='h-[480px] w-full px-16'>
-// 	<div
-// 		style={{
-// 			backgroundImage: `url('/takeaway/hero/hero-bg.png')`,
-// 			backgroundSize: 'cover',
-// 			backgroundRepeat: 'no-repeat',
-// 		}}
-// 		className='h-full w-full overflow-hidden'
-// 	>
-// 		<div
-// 			className='flex h-full w-full items-center justify-center bg-cover bg-no-repeat opacity-60'
-// 			style={{
-// 				background: `linear-gradient(78.23deg, #0B0600 74.17%, #4D412F 100.51%)`,
-// 			}}
-// 		>
-// 			<div className='space-y-2 text-[#EEDFC8]'>
-// 				<Breadcrumb
-// 					homeElement={'Home'}
-// 					separator={<span> {'>'} </span>}
-// 				/>
-// 			</div>
-// 		</div>
-// 	</div>
-// </div>
-// </section>
+export default Breadcrumb
