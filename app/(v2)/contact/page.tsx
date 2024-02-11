a4f630a99686020043c3acae95ba5adb465ec6b8
anikkdev <anikdev2016@gmail.com>
2024-02-11 20:17:19 +0600

    added branches

--- app/(v2)/contact/page.tsx
+++ app/(v2)/contact/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
-import DividerLine from '@/assets/homepage/divider_line.svg'
 import bg from '@/assets/career/career-and-offer-bg.png'
-import { Button } from '@/components/ui/button'
 import GradientWrapper from '@/components/v2/shared/gradient-wrapper'
 import BannerContents from '@/components/v2/shared/banner-contents'
 import DeliveryAndDine from '@/components/v2/home-components/header/DeliveryAndDine'
+import Branches from '@/components/v2/contact-us/branches'
 
 type Props = {}
 
@@ -17,7 +15,46 @@ const ContactUs = (props: Props) => {
         <BannerContents text='Contact Us' image={bg.src} />
       </div>
       <DeliveryAndDine />
-    </GradientWrapper>
+      {/* branches */}
+      <Branches branchOpeningHours={<>
+
+        <ul className='font-[400] text-[#727272] leading-[31.5px] font-open_sans tracking-[4%]'>
+          <li className="mb-1">Monday: 5:00 pm - 11:00 pm</li>
+          <li className="mb-1">Tuesday: 5:00 pm - 11:00 pm</li>
+          <li className="mb-1">Wednesday: 5:00 pm - 11:00 pm</li>
+          <li className="mb-1">Thursday: 5:00 pm - 11:00 pm</li>
+          <li className="mb-1">Friday: 5:00 pm - 11:00 pm</li>
+          <li className="mb-1">Saturday: 5:00 pm - 11:00 pm</li>
+          <li className="mb-1">Sunday: 5:00 pm - 11:00 pm</li>
+        </ul>
+      </>}
+        branchEmail='restaurant@holycowonline.com'
+        branchLocation='In Putney, London'
+        branchName='Holy Cow Indian Restaurant & Bar'
+        branchNum={1}
+
+      />
+
+
+
+      <Branches branchOpeningHours={<>
+        <ul className='font-[400] text-[#727272] leading-[31.5px] font-open_sans tracking-[4%]'>
+          <li className="mb-1">Monday: 2:00 pm – 11:00 pm</li>
+          <li className="mb-1">Tuesday: 2:00 pm – 11:00 pm</li>
+          <li className="mb-1">Wednesday: 2:00 pm – 11:00 pm</li>
+          <li className="mb-1">Thursday: 2:00 pm – 11:00 pm</li>
+          <li className="mb-1">Friday: 2:00 pm – 11:00 pm</li>
+          <li className="mb-1">Saturday: 2:00 pm – 11:00 pm</li>
+          <li className="mb-1">Sunday: 2:00 pm – 11:00 pm</li>
+        </ul></>}
+        branchEmail='canarywharf@holycowonline.com'
+        branchLocation='In canary Wharf, London'
+        branchName='Holy Cow Indian Restaurant & Bar'
+        branchNum={2}
+        classes='flex-row-reverse'
+
+      />
+    </GradientWrapper >
   )
 }
 export default ContactUs
