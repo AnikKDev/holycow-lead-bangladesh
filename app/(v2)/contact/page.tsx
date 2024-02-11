dc6051648dfd1c4a31cdb667f502c9d59444f6de
anikkdev <anikdev2016@gmail.com>
2024-02-11 20:38:28 +0600

    added takeaway section

--- app/(v2)/contact/page.tsx
+++ app/(v2)/contact/page.tsx
@@ -4,57 +4,66 @@ import GradientWrapper from '@/components/v2/shared/gradient-wrapper'
 import BannerContents from '@/components/v2/shared/banner-contents'
 import DeliveryAndDine from '@/components/v2/home-components/header/DeliveryAndDine'
 import Branches from '@/components/v2/contact-us/branches'
+import TakeawaySection from '@/components/v2/contact-us/takeaway-section'
 
 type Props = {}
 
 const ContactUs = (props: Props) => {
   return (
-    <GradientWrapper>
-      {/* banner here */}
+
+    <>
+
+      <GradientWrapper>
+        {/* banner here */}
+        <div>
+          <BannerContents text='Contact Us' image={bg.src} />
+        </div>
+        <DeliveryAndDine />
+        {/* branches */}
+      </GradientWrapper >
       <div>
-        <BannerContents text='Contact Us' image={bg.src} />
+
+        <Branches branchOpeningHours={<>
+
+          <ul className='font-[400] text-[#727272] leading-[31.5px] font-open_sans tracking-[4%]'>
+            <li className="mb-1">Monday: 5:00 pm - 11:00 pm</li>
+            <li className="mb-1">Tuesday: 5:00 pm - 11:00 pm</li>
+            <li className="mb-1">Wednesday: 5:00 pm - 11:00 pm</li>
+            <li className="mb-1">Thursday: 5:00 pm - 11:00 pm</li>
+            <li className="mb-1">Friday: 5:00 pm - 11:00 pm</li>
+            <li className="mb-1">Saturday: 5:00 pm - 11:00 pm</li>
+            <li className="mb-1">Sunday: 5:00 pm - 11:00 pm</li>
+          </ul>
+        </>}
+          branchEmail='restaurant@holycowonline.com'
+          branchLocation='In Putney, London'
+          branchName='Holy Cow Indian Restaurant & Bar'
+          branchNum={1}
+
+        />
+
+
+
+        <Branches branchOpeningHours={<>
+          <ul className='font-[400] text-[#727272] leading-[31.5px] font-open_sans tracking-[4%]'>
+            <li className="mb-1">Monday: 2:00 pm – 11:00 pm</li>
+            <li className="mb-1">Tuesday: 2:00 pm – 11:00 pm</li>
+            <li className="mb-1">Wednesday: 2:00 pm – 11:00 pm</li>
+            <li className="mb-1">Thursday: 2:00 pm – 11:00 pm</li>
+            <li className="mb-1">Friday: 2:00 pm – 11:00 pm</li>
+            <li className="mb-1">Saturday: 2:00 pm – 11:00 pm</li>
+            <li className="mb-1">Sunday: 2:00 pm – 11:00 pm</li>
+          </ul></>}
+          branchEmail='canarywharf@holycowonline.com'
+          branchLocation='In canary Wharf, London'
+          branchName='Holy Cow Indian Restaurant & Bar'
+          branchNum={2}
+          classes='flex-row-reverse'
+
+        />
+        <TakeawaySection />
       </div>
-      <DeliveryAndDine />
-      {/* branches */}
-      <Branches branchOpeningHours={<>
-
-        <ul className='font-[400] text-[#727272] leading-[31.5px] font-open_sans tracking-[4%]'>
-          <li className="mb-1">Monday: 5:00 pm - 11:00 pm</li>
-          <li className="mb-1">Tuesday: 5:00 pm - 11:00 pm</li>
-          <li className="mb-1">Wednesday: 5:00 pm - 11:00 pm</li>
-          <li className="mb-1">Thursday: 5:00 pm - 11:00 pm</li>
-          <li className="mb-1">Friday: 5:00 pm - 11:00 pm</li>
-          <li className="mb-1">Saturday: 5:00 pm - 11:00 pm</li>
-          <li className="mb-1">Sunday: 5:00 pm - 11:00 pm</li>
-        </ul>
-      </>}
-        branchEmail='restaurant@holycowonline.com'
-        branchLocation='In Putney, London'
-        branchName='Holy Cow Indian Restaurant & Bar'
-        branchNum={1}
-
-      />
-
-
-
-      <Branches branchOpeningHours={<>
-        <ul className='font-[400] text-[#727272] leading-[31.5px] font-open_sans tracking-[4%]'>
-          <li className="mb-1">Monday: 2:00 pm – 11:00 pm</li>
-          <li className="mb-1">Tuesday: 2:00 pm – 11:00 pm</li>
-          <li className="mb-1">Wednesday: 2:00 pm – 11:00 pm</li>
-          <li className="mb-1">Thursday: 2:00 pm – 11:00 pm</li>
-          <li className="mb-1">Friday: 2:00 pm – 11:00 pm</li>
-          <li className="mb-1">Saturday: 2:00 pm – 11:00 pm</li>
-          <li className="mb-1">Sunday: 2:00 pm – 11:00 pm</li>
-        </ul></>}
-        branchEmail='canarywharf@holycowonline.com'
-        branchLocation='In canary Wharf, London'
-        branchName='Holy Cow Indian Restaurant & Bar'
-        branchNum={2}
-        classes='flex-row-reverse'
-
-      />
-    </GradientWrapper >
+    </>
   )
 }
 export default ContactUs
---
+++ components/v2/contact-us/takeaway-section.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+export default function TakeawaySection({ }: Props) {
+  return (
+    <div className=" p-10 text-center">
+      <h2 className="text-[22px] font-[500] tracking-widest text-[#905A09]">Takeaway</h2>
+      <h1 className="mt-2 text-4xl font-[500] text-foreground">Holy Cow Indian Takeaway In London</h1>
+      <p className="mt-4 text-lg text-primary">
+        info@holycowonline.com{"\n      "}
+      </p>
+      <h3 className="mt-10 mb-4 text-4xl font-[500] text-foreground">Opening Hours</h3>
+      <div className="font-[400] text-[18px] leading-[31.5px] text-[#727272]">
+        <p className="">5:30pm to 11:00pm Monday – Thursday</p>
+        <p className="">5:00pm to 11:00pm Friday – Saturday</p>
+        <p className="">5:30pm to 10:30pm Sunday</p>
+      </div>
+      <Button className="mt-6 bg-[#d4a373] text-white hover:bg-[#b37659]">View Takeaway Location</Button>
+    </div>)
+}
