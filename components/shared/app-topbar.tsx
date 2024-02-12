a2a4ea0b89ce013da2bbdea0cc91d8acf2980a75
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-02-12 18:29:44 +0600

    checkout page color and spacing fixed

--- components/shared/app-topbar.tsx
+++ components/shared/app-topbar.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
 
+import { cn } from '@/lib/utils'
+
 import NewTopNav from './new-top-nav'
 
 const AppTopBar = () => {
@@ -14,7 +16,7 @@ const AppTopBar = () => {
 
 	useEffect(() => {
 		const handleScroll = () => {
-			if (window.scrollY > 0) {
+			if (window.scrollY > 0 && !pathname.includes('/checkout')) {
 				// Change the threshold as needed
 				navbarRef.current.classList.add('bg-primary')
 				navbarRef.current.classList.add('shadow-sm')
@@ -29,17 +31,33 @@ const AppTopBar = () => {
 		return () => window.removeEventListener('scroll', handleScroll)
 	}, [])
 
+	useEffect(() => {
+		if (pathname.includes('/checkout')) {
+		}
+	}, [pathname])
+
 	return (
 		<div className='relative'>
 			{/* <TopInformationBar /> */}
 			<div
 				ref={navbarRef}
-				className='fixed top-0 z-[1038] flex h-[80px] w-full items-center justify-center  text-primary-foreground mobile-md:h-16'
+				className={cn(
+					'fixed top-0 z-[1038] flex h-[80px] w-full items-center justify-center  text-primary-foreground mobile-md:h-16',
+					{
+						'border-b border-border bg-background':
+							pathname.includes('/checkout'),
+					}
+				)}
 			>
 				{/* <TopNav /> */}
 				<NewTopNav />
 			</div>
-			<div className='absolute bottom-0 left-0 right-0 top-0 z-[-1] h-[600px] bg-[linear-gradient(180deg,#E8D3A2_0%,#F3F2EB_102.18%,#FFF_102.19%)] '></div>
+			{pathname.includes('/checkout') ? (
+				<> </>
+			) : (
+				<div className='absolute bottom-0 left-0 right-0 top-0 z-[-1] h-[600px] bg-[linear-gradient(180deg,#E8D3A2_0%,#F3F2EB_102.18%,#FFF_102.19%)] '></div>
+			)}
+
 			<div className='h-[80px]'></div>
 		</div>
 	)
