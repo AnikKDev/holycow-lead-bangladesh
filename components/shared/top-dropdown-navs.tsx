79f4120d6e9bd533338cf73a6c0c5ccd2d09942b
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-02-12 14:39:11 +0600

    fine-dining-menu and lunch-menu route changes

--- components/shared/top-dropdown-navs.tsx
+++ components/shared/top-dropdown-navs.tsx
@@ -22,8 +22,8 @@ export function TopDropdownNavItesm() {
 					</NavigationMenuTrigger>
 					<NavigationMenuContent className='border-none bg-primary'>
 						<ul className='grid w-[250px]  gap-1.5 border-none bg-primary px-4 py-5'>
-							<ListItem href='/menu/dining-menu'>Fine Dining Menu</ListItem>
-							<ListItem href='/reservation'>Reservation</ListItem>
+							<ListItem href='/fine-dining-menu/'>Fine Dining Menu</ListItem>
+							<ListItem href='/reservations'>Reservation</ListItem>
 							<ListItem href='/locations/limehouse-canary-wharf-indian-restaurant/'>
 								Limehouse - Canary Wharf Restaurant
 							</ListItem>
