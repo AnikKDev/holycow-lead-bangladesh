0b732a647f5ac5e897a68ae6166634fa084de0e2
anikdev2016 <anikdev2016@gmail.com>
2024-02-12 08:04:00 +0600

    completed reservation section

---
+++ components/v2/reservation/happy-hours.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import SectionHeader from '../shared/SectionHeader'
+
+type Props = {}
+
+export default function HappyHours({}: Props) {
+	return (
+		<SectionHeader subTitle='Enjoy Fine-Dine' title='Happy Hours with Us' />
+	)
+}
