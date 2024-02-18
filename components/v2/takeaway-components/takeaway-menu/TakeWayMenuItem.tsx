936311f4ae31305842d6a9b26af141e958108b5e
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-18 16:35:36 +0600

    Fix takeaway menu component

--- components/v2/takeaway-components/takeaway-menu/TakeWayMenuItem.tsx
+++ components/v2/takeaway-components/takeaway-menu/TakeWayMenuItem.tsx
@@ -13,7 +13,7 @@ const TakeawayMenuItem = ({ title, img, url }: Props) => {
 		<article className='flex flex-col'>
 			<Image src={img} width={421} height={371} alt={title} />
 			<Link
-				className='v2-custom-button w-full bg-primary px-8 py-2 text-center font-lora text-xl font-semibold leading-[1.75]  tracking-[0.04em] text-foreground'
+				className='v2-custom-button w-full bg-primary p-2 text-center font-lora text-base font-semibold leading-[1.75] tracking-[0.04em]  text-foreground md:text-lg'
 				href={url}
 			>
 				{title}
--- components/v2/takeaway-components/takeaway-menu/TakeawayMenuList.tsx
+++ components/v2/takeaway-components/takeaway-menu/TakeawayMenuList.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const TakeWayMenuList = ({ takeawayMenuItems }: Props) => {
 	return (
-		<div className='lg:grid-cols-4 mb-36 grid grid-cols-1 gap-4 md:grid-cols-2 _desktop-sm:grid-cols-3 _desktop-md:grid-cols-4 _desktop-md:gap-8'>
+		<div className='lg:grid-cols-4 grid grid-cols-2 gap-4 md:grid-cols-3 _desktop-sm:grid-cols-3 _desktop-md:grid-cols-4 _desktop-md:gap-8'>
 			{takeawayMenuItems.map((item, index) => (
 				<TakeawayMenuItem
 					key={index}
