81e1af688e6f2aa563f3ee744fd887bf7d1ae86e
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-20 11:04:11 +0600

    reorder mobile menu and remove unnecessary items

--- components/shared/data.ts
+++ components/shared/data.ts
@@ -13,20 +13,18 @@ export const NAVBAR_ITEMS: NavItemType[] = [
 		path: '/',
 	},
 	{
-		title: 'Explore menu',
-		path: '/takeaway-menu',
+		title: 'Online Order',
+		path: '',
+		noPathIdentifier: 'online-order',
+	},
+	{
+		title: 'Takeaway',
+		path: '/takeaway',
 		submenu: true,
 		subMenuItems: [
-			{ title: 'Takeaway Menu', path: '/takeaway-menu' },
-			{ title: 'Lunch Menu', path: '/lunch-menu/' },
-			{
-				title: 'Fine Dine Menu',
-				path: '/fine-dining-menu/',
-			},
-			{
-				title: 'Make a Reservation',
-				path: '/locations/limehouse-canary-wharf-indian-restaurant/',
-			},
+			{ title: 'Takeaway Menu', path: '/takeaway-menu/' },
+			{ title: 'Takeaway Location', path: '/locations' },
+			{ title: 'Lunch Menu', path: '/lunch-menu' },
 		],
 	},
 	{
@@ -42,33 +40,76 @@ export const NAVBAR_ITEMS: NavItemType[] = [
 			},
 		],
 	},
-	{
-		title: 'Takeaway',
-		path: '/takeaway',
-		submenu: true,
-		subMenuItems: [
-			{ title: 'Takeaway Menu', path: '/takeaway-menu/' },
-			{ title: 'Takeaway Location', path: '/locations' },
-			{ title: 'Lunch Menu', path: '/lunch-menu' },
-		],
-	},
-	{
-		title: 'Online Order',
-		path: '',
-		noPathIdentifier: 'online-order',
-	},
+	{ title: 'Allergy Advice', path: '/allergy-advice' },
+	{ title: 'Offers', path: '/whats-on' },
 	{ title: 'Private Event', path: '/private-event' },
 	{ title: 'Career', path: '/career' },
-	{ title: 'Offers', path: '/whats-on' },
-	{ title: 'Contact Us', path: '/contact' },
-	{
-		title: 'Holy Cow',
-		path: '/about-us',
-		submenu: true,
-		subMenuItems: [
-			{ title: 'About Us', path: '/about-us' },
-			{ title: 'Contact Us', path: '/contact' },
-			{ title: 'Allergy Advice', path: '/allergy-advice' },
-		],
-	},
 ]
+
+// old menu
+
+// export const NAVBAR_ITEMS: NavItemType[] = [
+// 	{
+// 		title: 'Home',
+// 		path: '/',
+// 	},
+// 	{
+// 		title: 'Explore menu',
+// 		path: '/takeaway-menu',
+// 		submenu: true,
+// 		subMenuItems: [
+// 			{ title: 'Takeaway Menu', path: '/takeaway-menu' },
+// 			{ title: 'Lunch Menu', path: '/lunch-menu/' },
+// 			{
+// 				title: 'Fine Dine Menu',
+// 				path: '/fine-dining-menu/',
+// 			},
+// 			{
+// 				title: 'Make a Reservation',
+// 				path: '/locations/limehouse-canary-wharf-indian-restaurant/',
+// 			},
+// 		],
+// 	},
+// 	{
+// 		title: 'Restaurant',
+// 		path: '/restaurant',
+// 		submenu: true,
+// 		subMenuItems: [
+// 			{ title: 'Fine Dining Menu', path: '/fine-dining-menu/' },
+// 			{ title: 'Reservation', path: '/reservations/' },
+// 			{
+// 				title: 'Limehouse - Canary Wharf Restaurant',
+// 				path: '/locations/limehouse-canary-wharf-indian-restaurant/',
+// 			},
+// 		],
+// 	},
+// 	{
+// 		title: 'Takeaway',
+// 		path: '/takeaway',
+// 		submenu: true,
+// 		subMenuItems: [
+// 			{ title: 'Takeaway Menu', path: '/takeaway-menu/' },
+// 			{ title: 'Takeaway Location', path: '/locations' },
+// 			{ title: 'Lunch Menu', path: '/lunch-menu' },
+// 		],
+// 	},
+// 	{
+// 		title: 'Online Order',
+// 		path: '',
+// 		noPathIdentifier: 'online-order',
+// 	},
+// 	{ title: 'Private Event', path: '/private-event' },
+// 	{ title: 'Career', path: '/career' },
+// 	{ title: 'Offers', path: '/whats-on' },
+// 	{ title: 'Contact Us', path: '/contact' },
+// 	{
+// 		title: 'Holy Cow',
+// 		path: '/about-us',
+// 		submenu: true,
+// 		subMenuItems: [
+// 			{ title: 'About Us', path: '/about-us' },
+// 			{ title: 'Contact Us', path: '/contact' },
+// 			{ title: 'Allergy Advice', path: '/allergy-advice' },
+// 		],
+// 	},
+// ]
