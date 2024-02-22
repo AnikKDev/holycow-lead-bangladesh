f904b6fd8f1a88c363b1332f0730e38adb53f3a8
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-22 20:30:30 +0600

    Create contact slice

---
+++ redux/slices/contact/contactSlice.ts
@@ -0,0 +1,33 @@
+import { apiSlice } from '@/redux/services/apiSlice'
+
+// type ContactFormData = {
+// 	data: {
+// 		name?: string
+// 		contact_number?: string
+// 		email?: string
+// 		branch_email?:
+// 			| 'angel@holycowonline.com'
+// 			| 'archway@holycowonline.com'
+// 			| 'balham@holycowonline.com'
+// 			| 'battersea@holycowonline.com'
+// 			| 'hammersmith@holycowonline.com'
+// 			| 'kilburn@holycowonline.com'
+// 			| 'putney@holycowonline.com'
+// 			| 'canarywharf@holycowonline.com'
+// 		leave_a_note?: string
+// 	}
+// }
+
+export const contactApiSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		createAContactMail: builder.mutation({
+			query: (data) => ({
+				url: `/send_email`,
+				method: 'POST',
+				body: data,
+			}),
+		}),
+	}),
+})
+
+export const { useCreateAContactMailMutation } = contactApiSlice
